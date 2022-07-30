de3342224b6c018370b2a087358e22edd2fc7373
Ahmad Alghali <a.alghali@primeboard.org>
2022-07-30 04:37:49 +0100

    make endpoint public (no need for auth)

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from 'src/auth/decorators/public.decorator';
 
 @Controller(['health', ''])
 export class HealthController {
+  @Public()
   @Get()
   health(): string {
     return 'OK';
